57cff16389191c8181ccf3254b5c13cd71539935
AlexHadzhiev <72384702+AlexHadzhiev@users.noreply.github.com>
2021-11-03 14:26:40 +0200

    Delete api directory

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from api import views
-
-urlpatterns = [
-        path('', views.index, name='index'),
-        path('home', views.home, name='home'),
-        path('about', views.about, name='about'),
-        path('cars', views.cars, name='cars'),
-]
